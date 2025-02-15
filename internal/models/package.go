8967eb48dd1739ed1aba0ac9d81d7ce8e15dd9e1
Mahabub Rahman <mahabubrahman618@gmail.com>
2025-02-15 16:22:56 +0600

    Implement model for package

---
+++ internal/models/package.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type Package struct {
+	ID                int       `json:"id"`
+	PlatformID        int       `json:"platform_id" validate:"required"`
+	Name              string    `json:"name" validate:"required"`
+	Type              string    `json:"type" validate:"required,oneof=weekly monthly yearly"`
+	Duration          int       `json:"duration" validate:"required"`
+	Price             float64   `json:"price" validate:"required"`
+	Currency          string    `json:"currency" validate:"required"`
+	DiscountAmount    float64   `json:"discount_amount"`
+	DiscountExpiresAt time.Time `json:"discount_expires_at"`
+	CreatedAt         time.Time `json:"created_at"`
+	UpdatedAt         time.Time `json:"updated_at"`
+}
