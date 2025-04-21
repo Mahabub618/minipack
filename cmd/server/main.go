94ed7477da1e18f700b4e8a359dabfa660b7c67e
Mahabub <mahabub.rahman@tigerit.com>
2025-04-21 11:29:30 +0600

    Allowed CORS origin for vercel app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -56,7 +56,7 @@ func main() {
 	router := chi.NewRouter()
 
 	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:4200", "http://localhost:4000"}, // Allow Angular app
+		AllowedOrigins:   []string{"http://localhost:4200", "http://localhost:4000", "https://minipack-frontend.vercel.app"}, // Allow Angular app
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
